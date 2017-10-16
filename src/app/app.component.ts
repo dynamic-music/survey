39b37242b59de5e98d53606a1d110f898bd25a1f
Lucas Thompson <dev@lucas.im>
2017-10-16 17:58:25 +0100

    Wrap browser device motion events and wire up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Acceleration } from './app.module';
+import {Observable} from 'rxjs/Observable';
 
 import { PlayerComponent } from './player.component';
 @Component({
@@ -10,12 +12,21 @@ import { PlayerComponent } from './player.component';
 export class AppComponent {
   rootPage:any = PlayerComponent;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    @Inject('AccelerationObservable') private onOrientation: Observable<Acceleration>
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      onOrientation.subscribe((acc) => {
+        console.log(acc),
+        err => { console.error(err); }
+      });
     });
   }
 }
