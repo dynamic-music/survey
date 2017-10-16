8e219eef8bf0ec943dad3b11faa2d4958b85365f
Lucas Thompson <dev@lucas.im>
2017-10-16 18:25:43 +0100

    Start wiring up sensors in player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Acceleration } from './app.module';
-import {Observable} from 'rxjs/Observable';
 
 import { PlayerComponent } from './player.component';
 @Component({
@@ -15,18 +13,13 @@ export class AppComponent {
   constructor(
     platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    @Inject('AccelerationObservable') private onOrientation: Observable<Acceleration>
+    splashScreen: SplashScreen
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      onOrientation.subscribe((acc) => {
-        console.log(acc),
-        err => { console.error(err); }
-      });
     });
   }
 }
