6fe09446b71cb19ea668c05d1b6ef7f2744c6728
florianthalmann <florianthalmann@bluewin.ch>
2019-12-14 21:52:29 +0900

    added overpass service, improved geolocation service

---
+++ src/app/services/overpass.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+const API_URL = "http://overpass-api.de/api/interpreter?data=[out:json];";
+const DELTA = 0.004;
+
+interface OverpassObject {
+  amenity?: string,
+  nature?: string,
+  highway?: string,
+  railway?: string,
+  shop?: string,
+  place?: string,
+  historic?: string
+}
+
+@Injectable()
+export class OverpassService {
+
+  constructor(private http: Http) {}
+
+  async getObjectTagsNearby(lat: number, long: number): Promise<OverpassObject[]> {
+    const bbox = [lat-DELTA, long-DELTA, lat+DELTA, long+DELTA];
+    const query = API_URL+"node("+bbox.join(",")+");out;";
+    try {
+      const json = (await this.http.get(query).toPromise()).json();
+      return json.elements.filter(b => b.tags).map(b => b.tags);
+    } catch (e) {
+      console.error(e);
+      return Promise.reject(e.message || e);
+    }
+  }
+  
+  async getShopsAndAmenitiesNearby(lat: number, long: number): Promise<OverpassObject[]> {
+    return (await this.getObjectTagsNearby(lat, long))
+      .filter(o => o.amenity != null || o.shop != null);
+  }
+
+};
